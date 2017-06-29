99d4ea8a81ec446104bd156645263d8969e00db0
root <root@eggyolktech-sgp1-01>
2017-06-30 00:32:23 +0800

    Daily watch list files upload

---
+++ web/js/dailyWatcherUSIndexListData.js
@@ -0,0 +1,2 @@
+//2017-06-30 00:32:22.690782
+var dailyWatcherUSIndexListData ={"list": [{"label": "DAILY_20170630", "code": "DAILY_20170630", "list": [{"label": "/qdLBTYK.US @D [S7.0, M11.0, $101.28m]", "code": "LBTYK.US"}, {"label": "/qdHD.US @D [S17.0, M15.0, $615.89m]", "code": "HD.US"}, {"label": "/qdSRCL.US @D [S7.0, M2.0, $85.42m]", "code": "SRCL.US"}, {"label": "/qdNTAP.US @D [S8.0, M7.0, $120.99m]", "code": "NTAP.US"}, {"label": "/qdSHPG.US @D [S10.0, M8.0, $200.20m]", "code": "SHPG.US"}, {"label": "/qdLBTYA.US @D [S7.0, M8.0, $86.34m]", "code": "LBTYA.US"}]}]};
