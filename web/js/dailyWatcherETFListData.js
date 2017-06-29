1a3c3901b3f7d08552988ac31b1311684159ce5e
root <root@eggyolktech-sgp1-01>
2017-06-30 00:32:41 +0800

    Daily watch list files upload

--- web/js/dailyWatcherETFListData.js
+++ web/js/dailyWatcherETFListData.js
@@ -1,3 +1,2 @@
-//2017-06-29 22:57:27.585892
-var dailyWatcherETFListData ={"list": [{"label": "DAILY_20170629", "code": "DAILY_20170629", "list": []}]};//2017-06-29 23:14:25.697808
-var dailyWatcherETFListData ={"list": [{"label": "DAILY_20170629", "code": "DAILY_20170629", "list": []}]};
+//2017-06-30 00:32:40.928644
+var dailyWatcherETFListData ={"list": [{"label": "DAILY_20170630", "code": "DAILY_20170630", "list": []}]};
