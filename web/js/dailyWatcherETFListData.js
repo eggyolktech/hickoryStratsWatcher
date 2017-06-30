c4cff11d2203d8fa0f53d5dfc3c8f19b934513df
eggyolktech@gmail.com <eggyolktech@gmail.com>
2017-06-30 18:02:37 +0800

    Daily watch list files upload

--- web/js/dailyWatcherETFListData.js
+++ web/js/dailyWatcherETFListData.js
@@ -1,2 +1,2 @@
-//2017-06-30 17:56:39.603724
-var dailyWatcherETFListData ={"list": [{"code": "DAILY_ETFList_170630", "list": [], "label": "DAILY_ETFList_170630"}]};
+//2017-06-30 18:02:36.900939
+var dailyWatcherETFListData ={"list": [{"list": [], "code": "DAILY_ETFList_170630", "label": "DAILY_ETFList_170630"}]};
