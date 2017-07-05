8cd4e4d110a2fb733e96a432869603d3c002785e
eggyolktech@gmail.com <eggyolktech@gmail.com>
2017-07-06 02:04:19 +0800

    Daily watch list files commit all

--- web/js/dailyWatcherTrackListData.js
+++ web/js/dailyWatcherTrackListData.js
@@ -1,2 +1,2 @@
-//2017-07-06 02:03:37.923942
-var dailyWatcherTrackListData ={"list": [{"list": [{"label": "368.HK", "code": "368"}, {"label": "3993.HK", "code": "3993"}, {"label": "700.HK", "code": "700"}], "label": "DAILY_TrackList_170706_3", "code": "DAILY_TrackList_170706"}]};
+//2017-07-06 02:04:18.313176
+var dailyWatcherTrackListData ={"list": [{"code": "DAILY_TrackList_170706", "label": "DAILY_TrackList_170706_3", "list": [{"code": "3993", "label": "3993.HK"}, {"code": "368", "label": "368.HK"}, {"code": "700", "label": "700.HK"}]}]};
--- web/js/dailyWatcherUSTrackListData.js
+++ web/js/dailyWatcherUSTrackListData.js
@@ -1,2 +1,2 @@
-//2017-07-06 02:03:38.425613
-var dailyWatcherUSTrackListData ={"list": [{"list": [{"label": "FB.US", "code": "FB.US"}, {"label": "AMZN.US", "code": "AMZN.US"}, {"label": "GOOG.US", "code": "GOOG.US"}], "label": "DAILY_USTrackList_170706_3", "code": "DAILY_USTrackList_170706"}]};
+//2017-07-06 02:04:18.889436
+var dailyWatcherUSTrackListData ={"list": [{"code": "DAILY_USTrackList_170706", "label": "DAILY_USTrackList_170706_3", "list": [{"code": "GOOG.US", "label": "GOOG.US"}, {"code": "AMZN.US", "label": "AMZN.US"}, {"code": "FB.US", "label": "FB.US"}]}]};
