c40b3c283f7e34cd250b931d165c81690bfb2d13
eggyolktech <eggyolktech@gmail.com>
2018-05-24 18:45:39 +0800

    Daily watch list files commit all

--- web/js/dailyWatcherUSTrackListData.js
+++ web/js/dailyWatcherUSTrackListData.js
@@ -1,2 +1,2 @@
-//2018-05-24 18:41:16.588583
-var dailyWatcherUSTrackListData ={"list": [{"code": "DAILY_USTrackList_180524", "label": "DAILY_USTrackList_180524_3", "list": [{"code": "AMZN.US", "label": "AMZN.US"}, {"code": "FB.US", "label": "FB.US"}, {"code": "GOOG.US", "label": "GOOG.US"}]}]};
+//2018-05-24 18:45:39.675517
+var dailyWatcherUSTrackListData ={"list": [{"list": [{"code": "GOOG.US", "label": "GOOG.US"}, {"code": "AMZN.US", "label": "AMZN.US"}, {"code": "FB.US", "label": "FB.US"}], "code": "DAILY_USTrackList_180524", "label": "DAILY_USTrackList_180524_3"}]};
