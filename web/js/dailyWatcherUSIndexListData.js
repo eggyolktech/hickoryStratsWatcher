ecc31e69083778f31857cc1097dccf34c77a110a
eggyolktech <eggyolktech@gmail.com>
2018-09-08 00:55:05 +0800

    Daily watch list files commit all

--- web/js/dailyWatcherUSIndexListData.js
+++ web/js/dailyWatcherUSIndexListData.js
@@ -1,2 +1,2 @@
-//2018-09-07 00:57:12.403224
-var dailyWatcherUSIndexListData ={"list": [{"list": [{"code": "GD.US", "label": "<b>D [S17, M2, $285.65m, 0.1689]</b>"}, {"code": "BA.US", "label": "<b>D [S18, M1, $1335.44m, 0.1616]</b>"}, {"code": "BBBY.US", "label": "<b>D [S21, M2, $90.60m, 0.0085]</b>"}], "code": "DAILY_USIndexList_180907", "label": "DAILY_USIndexList_180907_3"}]};
+//2018-09-08 00:55:03.724245
+var dailyWatcherUSIndexListData ={"list": [{"code": "DAILY_USIndexList_180908", "list": [{"code": "SJM.US", "label": "<b>D [S2, M2, $137.23m, 0.2475]</b>"}, {"code": "GD.US", "label": "<b>D [S18, M3, $285.50m, 0.1315]</b>"}, {"code": "BBBY.US", "label": "<b>D [S22, M3, $90.39m, 0.0157]</b>"}, {"code": "HBI.US", "label": "<b>D [S1, M10, $129.17m, 0.0744]</b>"}, {"code": "IVZ.US", "label": "<b>D [S1, M12, $102.69m, 0.0131]</b>"}], "label": "DAILY_USIndexList_180908_5"}]};
--- web/js/dailyWatcherUSTrackListData.js
+++ web/js/dailyWatcherUSTrackListData.js
@@ -1,2 +1,2 @@
-//2018-09-07 00:57:13.871012
-var dailyWatcherUSTrackListData ={"list": [{"list": [{"code": "FB.US", "label": "FB.US"}, {"code": "AMZN.US", "label": "AMZN.US"}, {"code": "GOOG.US", "label": "GOOG.US"}], "code": "DAILY_USTrackList_180907", "label": "DAILY_USTrackList_180907_3"}]};
+//2018-09-08 00:55:05.253539
+var dailyWatcherUSTrackListData ={"list": [{"code": "DAILY_USTrackList_180908", "list": [{"code": "FB.US", "label": "FB.US"}, {"code": "AMZN.US", "label": "AMZN.US"}, {"code": "GOOG.US", "label": "GOOG.US"}], "label": "DAILY_USTrackList_180908_3"}]};
