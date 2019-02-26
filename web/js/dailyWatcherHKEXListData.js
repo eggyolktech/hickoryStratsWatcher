c9ab11374f930185d2d4201c3abb1bbd4674040b
eggyolktech <eggyolktech@gmail.com>
2019-02-26 23:26:45 +0800

    Daily watch list files commit all

--- web/js/dailyWatcherHKEXListData.js
+++ web/js/dailyWatcherHKEXListData.js
@@ -1,2 +1,2 @@
-//2019-02-25 23:51:12.378796
-var dailyWatcherHKEXListData ={"list": [{"code": "DAILY_HKEXList_190225", "list": [{"code": "8423", "label": "<b>D [S0, M4, $3.00m, 0.0059]</b>"}, {"code": "0996", "label": "<b>D [S3, M17, $6.47m, 0.0008]</b>"}, {"code": "8470", "label": "<b>D [S6, M0, $4.40m, 0.0000]</b>"}, {"code": "1240", "label": "<b>D [S12, M0, $4.94m, 0.0002]</b>"}, {"code": "0286", "label": "<b>D [S3, M5, $4.56m, 0.0002]</b>"}], "label": "DAILY_HKEXList_190225_5"}]};
+//2019-02-26 23:26:44.553579
+var dailyWatcherHKEXListData ={"list": [{"label": "DAILY_HKEXList_190226_5", "list": [{"label": "<b>D [S14, M0, $3.01m, 0.0533]</b>", "code": "1675"}, {"label": "<b>D [S0, M18, $5.02m, 0.0172]</b>", "code": "8526"}, {"label": "<b>D [S7, M1, $4.39m, 0.0017]</b>", "code": "8470"}, {"label": "<b>D [S0, M1, $4.93m, 0.0021]</b>", "code": "1240"}, {"label": "<b>D [S5, M0, $6.90m, 0.0003]</b>", "code": "1282"}], "code": "DAILY_HKEXList_190226"}]};
